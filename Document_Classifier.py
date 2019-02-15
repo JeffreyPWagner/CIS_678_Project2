5a79b24631e8f514f74d186fde00eb2b23ab6a15
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2019-02-15 13:11:09 -0500

    tweaked for 83.2%

--- Document_Classifier.py
+++ Document_Classifier.py
@@ -2,117 +2,135 @@ import collections
 import math
 from collections import Counter
 
-# set of all words in docs
-vocab = set()
 
-# list of all words
-allWords = []
+def naiveBayes (exclusionRate):
+    # set of all words in docs
+    vocab = set()
 
-# count of words in vocab
-countVocab = 0
+    # list of all words
+    allWords = []
 
-# list of all document labels
-allDocs = []
+    # count of words in vocab
+    countVocab = 0
 
-# set of all document labels
-docTypes = set()
+    # list of all document labels
+    allDocs = []
 
-# dictionary of count of each doc type
-docCount = {}
+    # set of all document labels
+    docTypes = set()
 
-# dictionary of probabilities of each doc type
-docProbabilities = {}
+    # dictionary of count of each doc type
+    docCount = {}
 
-# dictionary of concatenated docs of each type
-docCombined = {}
+    # dictionary of probabilities of each doc type
+    docProbabilities = {}
 
-# dictionary of word counts for each doc type plus count of words in vocab
-docWordCount = {}
+    # dictionary of concatenated docs of each type
+    docCombined = {}
 
-# dictionary of dictionaries of word probabilities for each doc type
-docWordProb = collections.defaultdict(dict)
+    # dictionary of word counts for each doc type plus count of words in vocab
+    docWordCount = {}
 
-# dictionary of minimum word probabilities for each doc type
-docMinProb = {}
+    # dictionary of dictionaries of word probabilities for each doc type
+    docWordProb = collections.defaultdict(dict)
 
-# total number of documents
-numDocs = 0
+    # dictionary of minimum word probabilities for each doc type
+    docMinProb = {}
 
-# list of test doc types
-testTypes = []
+    # total number of documents
+    numDocs = 0
 
-# list of assigned classes
-testGuesses = []
+    # list of test doc types
+    testTypes = []
 
-# label for training classes
-label = ""
+    # list of assigned classes
+    testGuesses = []
 
-# count of correct class assignments
-correctCount = 0
+    # label for training classes
+    label = ""
 
-# open and read training file
-trainingFile = open(r"C:\Users\jeffp\OneDrive\Documents\GitHub\CIS_678_Project2\forumTraining.data", "r")
+    # count of correct class assignments
+    correctCount = 0
 
-# read data from training file and close
-for row in trainingFile:
-    words = row.split()
-    label = words[0]
-    allDocs.append(label)
-    docTypes.add(label)
-    if label in docCombined.keys():
-        docCombined[label] += words
-    else:
-        docCombined[label] = words
-    del words[0]
-    allWords += words
-    for w in words:
-        vocab.add(w)
-trainingFile.close()
+    # open and read training file
+    trainingFile = open(r"C:\Users\jeffp\OneDrive\Documents\GitHub\CIS_678_Project2\forumTraining.data", "r")
 
-# count total words in vocab
-countVocab = len(vocab)
+    # read data from training file and close
+    for row in trainingFile:
+        words = row.split()
+        label = words[0]
+        allDocs.append(label)
+        docTypes.add(label)
+        if label in docCombined.keys():
+            docCombined[label] += words
+        else:
+            docCombined[label] = words
+        del words[0]
+        allWords += words
+        for w in words:
+            vocab.add(w)
+    trainingFile.close()
 
-# count total number of docs and count number of each type
-numDocs = len(allDocs)
-docCount = Counter(allDocs)
+    # count total words in vocab
+    countVocab = len(vocab)
 
-# calculate probabilities, word counts, and word probabilities for each doc type
-for doc in docTypes:
-    docProbabilities[doc] = docCount[doc] / numDocs
-    docWordCount[doc] = len(docCombined[doc]) + countVocab
-    wordcount = Counter(docCombined[doc])
-    docMinProb[doc] = 1 / docWordCount[doc]
-    for w in wordcount:
-        docWordProb[doc][w] = (wordcount[w] + 1) / docWordCount[doc]
+    # count total number of docs and count number of each type
+    numDocs = len(allDocs)
+    docCount = Counter(allDocs)
 
-mostCommon = [word for word, word_count in Counter(allWords).most_common(int(len(vocab) * .002))]
-
-testFile = open(r"C:\Users\jeffp\OneDrive\Documents\GitHub\CIS_678_Project2\forumTest.data", "r")
-
-for row in testFile:
-    words = row.split()
-    testTypes.append(words[0])
-    del words[0]
-    wordsMod = [word for word in words if word not in mostCommon]
-    maxProb = -999999999
-    # TODO set to min
-    guessClass = label
+    # calculate probabilities, word counts, and word probabilities for each doc type
     for doc in docTypes:
-        prob = math.log(docProbabilities[doc])
-        for word in wordsMod:
-            if word in docWordProb[doc].keys():
-                prob += math.log(docWordProb[doc][word])
-            else:
-                prob += math.log(docMinProb[doc])
-        if prob > maxProb:
-            maxProb = prob
-            guessClass = doc
-    testGuesses.append(guessClass)
+        docProbabilities[doc] = docCount[doc] / numDocs
+        docWordCount[doc] = len(docCombined[doc]) + countVocab
+        wordcount = Counter(docCombined[doc])
+        docMinProb[doc] = 1 / docWordCount[doc]
+        for w in wordcount:
+            docWordProb[doc][w] = (wordcount[w] + 1) / docWordCount[doc]
 
-for i, val in enumerate(testTypes):
-    if testGuesses[i] == val:
-        correctCount += 1
+    mostCommon = [word for word, word_count in Counter(allWords).most_common(int(len(vocab) * exclusionRate))]
 
-print(correctCount / len(testGuesses))
+    testFile = open(r"C:\Users\jeffp\OneDrive\Documents\GitHub\CIS_678_Project2\forumTest.data", "r")
+
+    for row in testFile:
+        words = row.split()
+        testTypes.append(words[0])
+        del words[0]
+        wordsMod = [word for word in words if word not in mostCommon]
+        wordsMod2 = [word for word in wordsMod if len(word) > 1]
+        maxProb = -999999999
+        # TODO set to min
+        guessClass = label
+        for doc in docTypes:
+            prob = math.log(docProbabilities[doc])
+            for word in wordsMod2:
+                if word in docWordProb[doc].keys():
+                    prob += math.log(docWordProb[doc][word])
+                else:
+                    prob += math.log(docMinProb[doc])
+            if prob > maxProb:
+                maxProb = prob
+                guessClass = doc
+        testGuesses.append(guessClass)
+
+    for i, val in enumerate(testTypes):
+        if testGuesses[i] == val:
+            correctCount += 1
+    print(correctCount / len(testGuesses))
+    return correctCount / len(testGuesses)
+
+
+def findOptimalExclusion():
+    exclusionRate = 0.001
+    maxAccuracy = -1
+    while exclusionRate < 0.003:
+        accuracy = naiveBayes(exclusionRate)
+        print(exclusionRate)
+        exclusionRate += 0.0002
+
+
+findOptimalExclusion()
 # TODO email prof to see if additional test sets are available to test for overtraining
 # TODO try additional steps in assignment
+# TODO write resulting lists to file
+# TODO refactor into function that find optimal common word exclusion
+# TODO potentially plot exclusion rates vs accuracy
